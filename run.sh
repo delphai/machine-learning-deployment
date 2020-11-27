96d0ff038071630997b651342b59102959980051
ahmed mahmoud <ahmed-delphai@ahmeds-Air.fritz.box>
2020-11-27 12:53:56 +0100

    add kubernetes and helm

--- run.sh
+++ run.sh
@@ -41,8 +41,50 @@ echo "Model $INPUT_BLOB_MODEL successfully downloaded."
 pip3 install --upgrade pip 
 pipenv lock -r > requirements.txt
 python3.8 -m pip install -r requirements.txt
-# 4 - Bundel the model
 
+# 4 - Bundel the model
 echo "BentoML Bundeling..."
 python3.8 /app/$INPUT_REPO_NAME/src/save.py
 echo "Successfully bundeled."
+
+# 5 - Build Docker Image 
+echo "Start Building BentoML image..."
+SAVED_PATH=$(bentoml get $INPUT_CLASS_NAME:latest --print-location --quiet)
+docker build -t $INPUT_REPO_NAME:latest ${SAVED_PATH}
+
+# 6 - Push docker image
+echo "Authenticating Docker..."
+REGISTRY_PASSWORD=$(az acr credential show -n delphaicommon | jq .passwords | jq '.[0]' | jq .value -r)
+REGISTRY_USERNAME=$(az acr credential show -n delphaicommon | jq .username -r)
+REGISTRY=delphaicommon.azurecr.io
+docker login ${REGISTRY} -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} 
+echo "Docker Successfilly authenticated."
+echo "Pushing Image to delphai registry..."
+docker push ${REGISTRY}/$INPUT_REPO_NAME:latest
+IMAGE=docker inspect --format='{{index .RepoDigests 0}}' delphaicommon.azurecr.io/$INPUT_REPO_NAME:latest
+
+# 7 - Set Kubernetes Kontext
+echo "Setting Kubernetes Kontext..."
+az aks get-credentials -n delphai-common -g tf-cluster 
+kubectl config current-context
+DOMAIN=$(kubectl get secret domain -o json --namespace default | jq .data.domain -r | base64 -d)
+
+# 8 - Deploy to kubernetes
+echo "Deploying to kubernetes..."
+kubectl create namespace $INPUT_REPO_NAME --output yaml --dry-run=client | kubectl apply -f -
+kubectl patch serviceaccount default --namespace $INPUT_REPO_NAME -p "{\"imagePullSecrets\": [{\"name\": \"acr-credentials\"}]}"
+helm repo add delphai https://delphai.github.io/helm-charts && helm repo update
+echo "Using helm delphai-knative service"
+    helm upgrade --install --wait --atomic \
+          $INPUT_REPO_NAME \
+          delphai/delphai-knative-service \
+          --namespace=$INPUT_REPO_NAME \
+          --set image=${IMAGE} \
+          --set httpPort=5000 \
+          --set grpcPort=8080 \
+          --set isPublic=true \
+          --set isUi=false \
+          --set domain=${DOMAIN} \
+          --set delphaiEnvironment=common
+
+kubectl patch deployment $INPUT_REPO_NAME --namespace $INPUT_REPO_NAME -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
