f99490bac32f4564a0c5746379dde06c677fe3c9
ahmed mahmoud <ahmed-delphai@ahmeds-Air.fritz.box>
2020-11-27 10:03:42 +0100

    use docker image from registry

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,3 @@
-FROM teracy/ubuntu:18.04-dind-latest
-WORKDIR /app
-RUN curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.19.0/bin/linux/amd64/kubectl
-RUN curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-RUN chmod 700 get_helm.sh
-RUN  ./get_helm.sh
-RUN apt update && apt install software-properties-common -y
-RUN add-apt-repository ppa:deadsnakes/ppa -y
-RUN apt install python3.8 -y
-RUN apt update && apt install python3-pip -y
-RUN pip3 --version
-RUN pip3 install bentoml
+FROM delphairegistry/dind-az-kctl-helm:v1
 COPY . /app
-ENTRYPOINT [ "bash", "/app/run.sh" ]
+CMD /app/run.sh
--- run.sh
+++ run.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 set -e
-
 # Variables
-bold=$(tput bold)
 REPO_URL="https://ahmedmahmo:$INPUT_GITHUB_TOKEN@github.com/delphai/$INPUT_REPO_NAME"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # 1 - Authenticate to Azure
-echo "${bold}Authenticating to Azure...."
+echo "Authenticating to Azure...."
 az login --service-principal --username $INPUT_CLIENT_ID --password $INPUT_CLIENT_SECRET --tenant $INPUT_TENANT_ID
 echo "Azure Authentication complete."
 
 # Check Subscription
-echo "${bold}Checking Subscription..."
+echo "Checking Subscription..."
 SUBSCRIPTION=$(az account show | jq .name -r)
 if [ "$SUBSCRIPTION" == "common" ]; then
     echo "Subscription is ${SUBSCRIPTION}"
 else
-    echo "${bold}Subscription not set correctly -> Exit"
+    echo "Subscription not set correctly -> Exit"
     exit
 fi
 
 # 2 - Clone Repo 
-echo "${bold}cloning $INPUT_REPO_NAME...."
+echo "cloning $INPUT_REPO_NAME...."
 git clone ${REPO_URL}
 echo "$INPUT_REPO_NAME cloned."
 
@@ -32,7 +30,7 @@ cd "${DIR}/$INPUT_REPO_NAME" || exit
 
 # 3 - Download the Model
 echo "Given Model name is $INPUT_BLOB_MODEL"
-echo "${bold}Downloading Model from Azure blob..."
+echo "Downloading Model from Azure blob..."
 az storage blob download-batch -d . -s $INPUT_BLOB_MODEL --account-name tfdelphaicommon --connection-string $INPUT_CONNECTION_STRING
 echo "Model $INPUT_BLOB_MODEL successfully downloaded."
 
@@ -40,4 +38,4 @@ echo "Model $INPUT_BLOB_MODEL successfully downloaded."
 
 echo "BentoML Bundeling..."
 python3.8 /app/$INPUT_REPO_NAME/src/save.py
-echo "${bold}Successfully bundeled."
+echo "Successfully bundeled."
